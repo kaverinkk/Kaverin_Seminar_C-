50a91804933707588da37ddfc35d88e8e90a3d4b
Andrey <kaverin97andry@mail.ru>
2022-04-29 14:16:59 +0300

    Add task003

---
+++ task0/task01/task002/task003/Program.cs
@@ -0,0 +1,9 @@
+﻿/* Напишите программу вычисления значения
+функции возведения числа в квадрат
+4 - > 16
+3 - > 9 */
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = (number*number);
+Console.WriteLine(result);
