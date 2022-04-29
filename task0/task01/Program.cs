c0a6f2f1f9ffa80027aa7ae6dc86de1c53fd554a
Andrey <kaverin97andry@mail.ru>
2022-04-29 11:53:16 +0300

    Add task001

---
+++ task0/task01/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая принимает на вх 2 числа и проверяет, является ли 1 число квадратом 2.
+Например: 
+a = 5; b = 25 -> да;
+a = 7; b = 20 -> нет
+*/
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a * a == b)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}    
+
+
