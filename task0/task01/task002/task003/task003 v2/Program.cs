4faa5fda32a7b6b1f7d8c40e5a8e13724d0addc6
Andrey <kaverin97andry@mail.ru>
2022-04-29 15:04:08 +0300

    Add task003 v2

---
+++ task0/task01/task002/task003/task003 v2/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, кот на входе принимает число n, а на выходе
+показывает все целые числа в промеж от -n до n*/
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = - number; i <= number; i++)
+{
+    Console.Write(i);
+    if (i != number)
+    {
+        Console.Write(", ");
+    }
+    
+}
+ 
